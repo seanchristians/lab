ab0fc98ef9a981ab34538a9ae1d079296aa9049c
Sean Christians <security@seanchristians.com>
2025-11-16 19:15:52 -0500

    Tailscale secrets implemented in other PR

--- spacelift/tailscale.secrets.tf
+++
@@ -1,9 +0,0 @@
-resource "spacelift_environment_variable" "tailscale_oauth_client_id" {
-  name     = "TF_VAR_tailscale_oauth_client_id"
-  stack_id = spacelift_stack.tailscale.id
-}
-
-resource "spacelift_environment_variable" "tailscale_oauth_client_secret" {
-  name     = "TF_VAR_tailscale_oauth_client_secret"
-  stack_id = spacelift_stack.tailscale.id
-}
