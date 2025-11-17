cf552d51b69e3b5bd5f3d863600d16bf096b9ae0
Sean Christians <security@seanchristians.com>
2025-11-16 19:09:22 -0500

    Move secrets to spacelift stack

--- spacelift/tailscale.secrets.tf
+++ spacelift/tailscale.secrets.tf
@@ -1,13 +1,9 @@
-data "spacelift_stack" "tailscale" {
-  stack_id = "tailscale"
-}
-
 resource "spacelift_environment_variable" "tailscale_oauth_client_id" {
   name     = "TF_VAR_tailscale_oauth_client_id"
-  stack_id = data.spacelift_stack.tailscale.id
+  stack_id = spacelift_stack.tailscale.id
 }
 
 resource "spacelift_environment_variable" "tailscale_oauth_client_secret" {
   name     = "TF_VAR_tailscale_oauth_client_secret"
-  stack_id = data.spacelift_stack.tailscale.id
+  stack_id = spacelift_stack.tailscale.id
 }
