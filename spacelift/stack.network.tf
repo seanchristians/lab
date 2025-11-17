db801b8576fb1063406bef605040ce8190cb730c
Sean Christians <security@seanchristians.com>
2025-11-16 21:40:52 -0500

    import tailscale stack to network stack before destroy

--- spacelift/stack.network.tf
+++ spacelift/stack.network.tf
@@ -10,3 +10,21 @@ resource "spacelift_stack" "network" {
   manage_state = true
   space_id     = spacelift_space.production.id
 }
+
+resource "spacelift_stack" "tailscale" {
+  branch       = "main"
+  name         = "network"
+  repository   = "lab"
+  project_root = "network"
+
+  allow_run_promotion = true
+  autodeploy          = true
+
+  manage_state = true
+  space_id     = spacelift_space.production.id
+}
+
+import {
+  to = spacelift_stack.network
+  id = spacelift_stack.tailscale.id
+}
