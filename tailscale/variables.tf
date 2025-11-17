7ec2f0b4e36cc8127554d5c5cfc8d573115dc4b9
Sean Christians <security@seanchristians.com>
2025-11-16 19:18:23 -0500

    Tailscale provider (#3)

---
+++ tailscale/variables.tf
@@ -0,0 +1,13 @@
+variable "tailscale_tailnet_id" {
+  type = string
+}
+
+variable "tailscale_oauth_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "tailscale_oauth_client_secret" {
+  type      = string
+  sensitive = true
+}
