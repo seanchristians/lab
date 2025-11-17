f807526cb2f438419289b861e3ffc61257e94f86
Sean Christians <security@seanchristians.com>
2025-11-16 21:51:23 -0500

    Remove tailscale stack

--- spacelift/stack.network.tf
+++ spacelift/stack.network.tf
@@ -10,16 +10,3 @@ resource "spacelift_stack" "network" {
   manage_state = true
   space_id     = spacelift_space.production.id
 }
-
-resource "spacelift_stack" "tailscale" {
-  branch       = "main"
-  name         = "tailscale"
-  repository   = "lab"
-  project_root = "tailscale"
-
-  allow_run_promotion = true
-  autodeploy          = true
-
-  manage_state = true
-  space_id     = spacelift_space.production.id
-}
