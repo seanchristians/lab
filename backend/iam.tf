d22f64bd9d8325aaf3a1626932dffa1e7a3ac98b
Sean Christians <security@seanchristians.com>
2025-11-19 23:43:21 -0500

    Full permissions to IAM

--- backend/iam.tf
+++ backend/iam.tf
@@ -1,8 +1,8 @@
 data "aws_iam_policy_document" "github_actions_assume_role" {
   statement {
     principals {
-      type        = "Federated"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -21,34 +21,7 @@ resource "aws_iam_role" "terraform" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
-data "aws_iam_policy_document" "terraform" {
-  statement {
-    actions   = ["ecr:GetAuthorizationToken"]
-    resources = ["*"]
-  }
-
-  statement {
-    actions = [
-      "s3:CreateBucket",
-      "s3:ListAllMyBuckets",
-      "s3:GetBucketLocation",
-      "s3:ListBucket"
-    ]
-    resources = [
-      "arn:aws:s3:::*"
-    ]
-  }
-
-  statement {
-    actions = ["s3:*Object"]
-    resources = [
-      "arn:aws:s3:::*/*"
-    ]
-  }
-}
-
-resource "aws_iam_role_policy" "terraform" {
-  name   = "GitHubActionsTerraformPolicy"
-  policy = data.aws_iam_policy_document.terraform.json
-  role   = aws_iam_role.terraform.name
+resource "aws_iam_role_policy_attachment" "terraform" {
+  role       = aws_iam_role.terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
 }
