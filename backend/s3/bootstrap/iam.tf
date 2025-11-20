e4abc1d112467b04f35bf5d6df075ce36a42fb32
Sean Christians <security@seanchristians.com>
2025-11-20 09:08:48 -0500

    s3 permissions for terraform state bucket

--- backend/s3/bootstrap/iam.tf
+++ backend/s3/bootstrap/iam.tf
@@ -26,7 +26,20 @@ resource "aws_iam_role" "terraform" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "terraform" {
+resource "aws_iam_role_policy_attachment" "terraform_iam" {
   role       = aws_iam_role.terraform.name
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+data "aws_iam_policy_document" "terraform_s3" {
+  statement {
+    actions   = ["s3:*"]
+    resources = [aws_s3_bucket.terraform_state.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "terraform_s3" {
+  name   = "terraform-s3"
+  policy = data.aws_iam_policy_document.terraform_s3.json
+  role   = aws_iam_role.terraform.name
 }
