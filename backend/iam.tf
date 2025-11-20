2805cd5bbb0796a12ffaa06813bcf9c9d92f3190
Sean Christians <security@seanchristians.com>
2025-11-19 23:30:54 -0500

    Basic IAM to access s3 for state management

--- backend/iam.tf
+++ backend/iam.tf
@@ -1,9 +1,18 @@
 data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
 
-}
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
-data "aws_iam_policy_document" "terraform" {
-
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:seanchristians/lab:*"]
+    }
+  }
 }
 
 resource "aws_iam_role" "terraform" {
@@ -12,12 +21,34 @@ resource "aws_iam_role" "terraform" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
-resource "aws_iam_policy" "terraform" {
-  name   = "GitHubActionsTerraformPolicy"
-  policy = data.aws_iam_policy_document.terraform.json
+data "aws_iam_policy_document" "terraform" {
+  statement {
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "s3:CreateBucket",
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+  }
+
+  statement {
+    actions = ["s3:*Object"]
+    resources = [
+      "arn:aws:s3:::*/*"
+    ]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "terraform" {
-  role       = aws_iam_role.terraform.name
-  policy_arn = aws_iam_policy.terraform.arn
+resource "aws_iam_role_policy" "terraform" {
+  name   = "GitHubActionsTerraformPolicy"
+  policy = data.aws_iam_policy_document.terraform.json
+  role   = aws_iam_role.terraform.name
 }
