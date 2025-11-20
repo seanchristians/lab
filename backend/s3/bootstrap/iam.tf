69e930565af47d20e0243e84402422f890f675ed
Sean Christians <security@seanchristians.com>
2025-11-20 00:48:17 -0500

    OIDC provider in IAM for GitHub

--- backend/s3/bootstrap/iam.tf
+++ backend/s3/bootstrap/iam.tf
@@ -1,3 +1,8 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+}
+
 data "aws_iam_policy_document" "github_actions_assume_role" {
   statement {
     principals {
