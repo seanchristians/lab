54fde103ced95c5a65470fb40d1f5b9b406d0b8d
Sean Christians <security@seanchristians.com>
2025-11-20 00:21:23 -0500

    s3 general access policy within state bucket

--- backend/s3/iam.s3.tf
+++ backend/s3/iam.s3.tf
@@ -10,8 +10,25 @@ data "aws_iam_policy_document" "tf_state_init_s3" {
   }
 }
 
+data "aws_iam_policy_document" "tf_state" {
+  statement {
+    actions = [
+      "s3:*"
+    ]
+    resources = [
+      aws_s3_bucket.terraform_state.arn
+    ]
+  }
+}
+
 resource "aws_iam_role_policy" "tf_state_init_s3" {
   name   = "terraform-create-s3-bucket"
   policy = data.aws_iam_policy_document.tf_state_init_s3.json
+  role   = aws_iam_role.terraform.name
+}
+
+resource "aws_iam_role_policy" "tf_state_s3" {
+  name   = "terraform-manage-state-s3"
+  policy = data.aws_iam_policy_document.tf_state.json
   role   = aws_iam_role.terraform.name
 }
--- backend/s3/s3.tf
+++ backend/s3/s3.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = "terraform-state"
   force_destroy = true
+
+  depends_on = [ aws_iam_role_policy.tf_state_init_s3 ]
 }
