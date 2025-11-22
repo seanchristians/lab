eaf12705800eb4a18018b4143df7acc1bbde9288
Sean Christians <security@seanchristians.com>
2025-11-22 10:37:21 -0500

    GitHub expects repo name only
    Not the canonical namespace

--- infrastructure/backends/s3/github.tf
+++ infrastructure/backends/s3/github.tf
@@ -1,10 +1,10 @@
 resource "github_repository_environment" "terraform" {
   environment = "terraform"
-  repository  = var.github_repo
+  repository  = "lab"
 }
 
 resource "github_actions_environment_variable" "aws_assume_role_arn" {
-  repository    = var.github_repo
+  repository    = "lab"
   environment   = github_repository_environment.terraform.environment
   variable_name = "AWS_ROLE_ARN"
   value         = aws_iam_role.terraform.arn
