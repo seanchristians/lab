1d41e34c5ad17a36867023b506da60bf491ecdc1
Sean Christians <security@seanchristians.com>
2025-11-22 09:16:14 -0500

    s3 bucket versioning

--- infrastructure/backends/s3/s3.tf
+++ infrastructure/backends/s3/s3.tf
@@ -2,3 +2,15 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket        = "terraform-state"
   force_destroy = true
 }
+
+resource "aws_s3_bucket_acl" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
