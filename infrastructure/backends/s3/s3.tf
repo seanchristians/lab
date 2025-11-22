e3f70d1a166f6fd82acc043ce581e0b0e1833f30
Sean Christians <security@seanchristians.com>
2025-11-22 09:22:38 -0500

    Delete noncurrent s3 object versions after 30 days

--- infrastructure/backends/s3/s3.tf
+++ infrastructure/backends/s3/s3.tf
@@ -14,3 +14,14 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    id = "version-history"
+    status = "Enabled"
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
