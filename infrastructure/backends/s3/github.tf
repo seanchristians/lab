99d57887aa4297d16765581a686494e70aac8ce4
Sean Christians <security@seanchristians.com>
2025-11-22 09:41:11 -0500

    Set repo name with variable

--- infrastructure/backends/s3/github.tf
+++ infrastructure/backends/s3/github.tf
@@ -1,10 +1,10 @@
 resource "github_repository_environment" "terraform" {
   environment = "terraform"
-  repository  = "seanchristians/lab"
+  repository  = var.github_repo
 }
 
 resource "github_actions_environment_variable" "aws_assume_role_arn" {
-  repository    = "seanchristians/lab"
+  repository    = var.github_repo
   environment   = github_repository_environment.terraform.environment
   variable_name = "AWS_ROLE_ARN"
   value         = aws_iam_role.terraform.arn
--- infrastructure/backends/s3/iam.tf
+++ infrastructure/backends/s3/iam.tf
@@ -15,14 +15,13 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["seanchristians/lab:*"]
+      values   = ["${var.github_repo}:*"]
     }
   }
 }
 
 resource "aws_iam_role" "terraform" {
   name               = "GitHubActionsTerraformRole"
-  description        = "Terraform in github.com/seanchristians/lab"
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
