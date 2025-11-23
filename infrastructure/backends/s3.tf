764c3db3a76fafa1794da49e628f4097b354329b
Sean Christians <security@seanchristians.com>
2025-11-22 19:50:58 -0500

    Switch to correct bucket

--- infrastructure/backends/s3.tf
+++ infrastructure/backends/s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state"
+    bucket       = "seanchristians-lab-terraform-state"
     key          = ""
     region       = "ca-central-1"
     use_lockfile = true
--- infrastructure/github/backend.tf
+++ infrastructure/github/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state"
+    bucket       = "seanchristians-lab-terraform-state"
     key          = "github/terraform.tfstate"
     region       = "ca-central-1"
     use_lockfile = true
