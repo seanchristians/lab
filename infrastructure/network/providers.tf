387034f7b08a4c46fda82e28995d1b3d4c0de0f5
Sean Christians <security@seanchristians.com>
2025-11-23 00:58:29 -0500

    Set up Tailscale with Terraform (#25)

--- infrastructure/network/providers.tf
+++ infrastructure/network/providers.tf
@@ -8,9 +8,5 @@ terraform {
 }
 
 provider "tailscale" {
-  tailnet             = var.tailscale_tailnet_id
-  oauth_client_id     = var.tailscale_oauth_client_id
-  oauth_client_secret = var.tailscale_oauth_client_secret
-  user_agent          = "Terraform / seanchristians/lab"
-  scopes              = ["all"]
+  user_agent = "Terraform / seanchristians/lab"
 }
