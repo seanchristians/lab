a9ed898c1584f92bc73b8fc41263ab825886f349
Sean Christians <security@seanchristians.com>
2025-11-23 00:43:57 -0500

    Set AWS Role ARN in dev and prod envs

--- infrastructure/backend/github.tf
+++ infrastructure/backend/github.tf
@@ -2,18 +2,6 @@ data "github_user" "seanchristians" {
   username = "seanchristians"
 }
 
-resource "github_repository_environment" "terraform" {
-  environment = "terraform"
-  repository  = "lab"
-}
-
-resource "github_actions_environment_variable" "aws_assume_role_arn" {
-  repository    = "lab"
-  environment   = github_repository_environment.terraform.environment
-  variable_name = "AWS_ROLE_ARN"
-  value         = aws_iam_role.terraform.arn
-}
-
 resource "github_repository_environment" "development" {
   environment         = "development"
   repository          = "lab"
@@ -30,4 +18,18 @@ resource "github_repository_environment" "production" {
   reviewers {
     users = [data.github_user.seanchristians.id]
   }
+}
+
+resource "github_actions_environment_variable" "aws_assume_role_arn_development" {
+  repository    = "lab"
+  environment   = github_repository_environment.development.environment
+  variable_name = "AWS_ROLE_ARN"
+  value         = aws_iam_role.terraform.arn
+}
+
+resource "github_actions_environment_variable" "aws_assume_role_arn_production" {
+  repository    = "lab"
+  environment   = github_repository_environment.production.environment
+  variable_name = "AWS_ROLE_ARN"
+  value         = aws_iam_role.terraform.arn
 }
