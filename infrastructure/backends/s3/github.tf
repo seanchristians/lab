355066d9c730e6eb8902698f1d0fe45219f20796
Sean Christians <security@seanchristians.com>
2025-11-22 20:21:17 -0500

    Move github modifications to backend config

--- infrastructure/backends/s3/github.tf
+++ infrastructure/backends/s3/github.tf
@@ -1,3 +1,7 @@
+data "github_user" "seanchristians" {
+  username = "seanchristians"
+}
+
 resource "github_repository_environment" "terraform" {
   environment = "terraform"
   repository  = "lab"
@@ -8,4 +12,22 @@ resource "github_actions_environment_variable" "aws_assume_role_arn" {
   environment   = github_repository_environment.terraform.environment
   variable_name = "AWS_ROLE_ARN"
   value         = aws_iam_role.terraform.arn
+}
+
+resource "github_repository_environment" "development" {
+  environment         = "development"
+  repository          = "lab"
+  can_admins_bypass   = true
+  prevent_self_review = false
+}
+
+resource "github_repository_environment" "production" {
+  environment         = "production"
+  repository          = "lab"
+  can_admins_bypass   = true
+  prevent_self_review = false
+
+  reviewers {
+    users = [data.github_user.seanchristians.id]
+  }
 }
--- infrastructure/github/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "seanchristians-lab-terraform-state"
-    key          = "github/terraform.tfstate"
-    region       = "ca-central-1"
-    use_lockfile = true
-  }
-}
