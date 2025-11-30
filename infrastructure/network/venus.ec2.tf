4a40aa22c1c0ccdf84be7ec54a96085f5bca3c2a
Sean Christians <security@seanchristians.com>
2025-11-30 12:10:21 -0500

    Amazon Linux 2023 AMI

--- infrastructure/network/venus.ec2.tf
+++ infrastructure/network/venus.ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "venus" {
+  ami                 = data.aws_ami.amazon_linux_latest.id
   instance_type       = "t4g.nano"
   subnet_id           = aws_default_subnet.cac1-az4.id
   enable_primary_ipv6 = true
@@ -9,6 +10,20 @@ resource "aws_instance" "venus" {
 
   tags = {
     Name = "Venus"
+  }
+}
+
+data "aws_ami" "amazon_linux_latest" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-arm64"]
   }
 }
 
