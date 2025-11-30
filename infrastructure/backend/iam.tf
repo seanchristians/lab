22ff436e9283f34c69752ba14b69fe289c50a22f
Sean Christians <security@seanchristians.com>
2025-11-30 11:26:49 -0500

    Give terraform full admin permissions in aws

--- infrastructure/backend/iam.tf
+++ infrastructure/backend/iam.tf
@@ -25,25 +25,9 @@ resource "aws_iam_role" "terraform" {
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "terraform_iam" {
+resource "aws_iam_role_policy_attachment" "terraform_admin" {
   role       = aws_iam_role.terraform.name
-  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-}
-
-data "aws_iam_policy_document" "terraform_s3" {
-  statement {
-    actions   = ["s3:*"]
-    resources = [
-      aws_s3_bucket.terraform_state.arn,
-      "${aws_s3_bucket.terraform_state.arn}/*"
-    ]
-  }
-}
-
-resource "aws_iam_role_policy" "terraform_s3" {
-  name   = "terraform-s3"
-  policy = data.aws_iam_policy_document.terraform_s3.json
-  role   = aws_iam_role.terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 output "IAM_ROLE_ARN" {
