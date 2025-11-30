ada59ac636058e33290b69cec0813790ec380896
Sean Christians <security@seanchristians.com>
2025-11-30 12:37:22 -0500

    Use existing AMI, don't destroy the resource

--- infrastructure/network/venus.ec2.tf
+++ infrastructure/network/venus.ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "venus" {
-  ami                 = "ami-0c9146af31980eb75"
+  ami                 = "ami-0c096d78166072826"
   instance_type       = "t4g.nano"
   availability_zone   = aws_default_subnet.cac1-az4.availability_zone
   enable_primary_ipv6 = true
