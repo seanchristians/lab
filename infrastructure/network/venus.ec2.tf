0f86942f63268de99cc65783a97b226f812a37b9
Sean Christians <security@seanchristians.com>
2025-11-30 12:42:12 -0500

    remove EBS volume, it is the root block device

--- infrastructure/network/venus.ec2.tf
+++ infrastructure/network/venus.ec2.tf
@@ -13,27 +13,7 @@ resource "aws_instance" "venus" {
   }
 }
 
-resource "aws_ebs_volume" "venus" {
-  availability_zone = aws_default_subnet.cac1-az4.availability_zone
-  encrypted         = true
-  size              = "8"
-  type              = "gp3"
-  iops              = "3000"
-  throughput        = "125"
-}
-
-resource "aws_volume_attachment" "venus" {
-  instance_id = aws_instance.venus.id
-  volume_id   = aws_ebs_volume.venus.id
-  device_name = "/dev/xvda"
-}
-
 import {
   to = aws_instance.venus
   id = "i-03250ab66e443ceba"
-}
-
-import {
-  to = aws_ebs_volume.venus
-  id = "vol-086f93c1a420c9722"
 }
