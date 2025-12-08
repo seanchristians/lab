cc4c5dfb12f1bbc5f016fbe7a94c845a4e69f58a
Sean Christians <security@seanchristians.com>
2025-12-07 23:43:23 -0500

    Remove TLS provider

--- infrastructure/acme/certificates.tf
+++ infrastructure/acme/certificates.tf
@@ -1,19 +1,6 @@
-ephemeral "tls_private_key" "veronica" {
-  algorithm = "ED25519"
-}
-
-resource "tls_cert_request" "veronica" {
-  private_key_pem = ephemeral.tls_private_key.veronica.private_key_pem
-  dns_names       = ["veronica.${var.domain}"]
-
-  subject {
-    common_name = "veronica.${var.domain}"
-  }
-}
-
 resource "acme_certificate" "veronica" {
-  account_key_pem         = acme_registration.letsencrypt.account_key_pem
-  certificate_request_pem = tls_cert_request.veronica.cert_request_pem
+  account_key_pem = acme_registration.letsencrypt.account_key_pem
+  common_name     = "veronica.${var.domain}"
 
   dns_challenge {
     provider = "porkbun"
--- infrastructure/acme/providers.tf
+++ infrastructure/acme/providers.tf
@@ -4,15 +4,9 @@ terraform {
       source  = "vancluever/acme"
       version = "2.39.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.1.0"
-    }
   }
 }
 
 provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
-
-provider "tls" {}
