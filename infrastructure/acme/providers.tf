a971ca08a0778677efe9d9c7d8ff47966a1c2a9a
Sean Christians <security@seanchristians.com>
2025-12-07 22:32:07 -0500

    TLS provider

--- infrastructure/acme/providers.tf
+++ infrastructure/acme/providers.tf
@@ -4,9 +4,15 @@ terraform {
       source  = "vancluever/acme"
       version = "2.39.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
   }
 }
 
 provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
+
+provider "tls" {}
