bfe4e4ff33317f8a1da549013ea441751541eabb
Sean Christians <security@seanchristians.com>
2025-12-07 22:48:32 -0500

    csr for veronica.scchq.net

---
+++ infrastructure/acme/certificates.tf
@@ -0,0 +1,17 @@
+resource "tls_cert_request" "veronica" {
+  private_key_pem = ""
+  dns_names       = ["veronica.${var.domain}"]
+
+  subject {
+    common_name = "veronica.${var.domain}"
+  }
+}
+
+resource "acme_certificate" "veronica" {
+  account_key_pem         = acme_registration.letsencrypt.account_key_pem
+  certificate_request_pem = tls_cert_request.veronica.cert_request_pem
+
+  dns_challenge {
+    provider = "porkbun"
+  }
+}
