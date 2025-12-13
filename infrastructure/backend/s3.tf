ee0dc9f4aa41496db706dc1cc659723da2566981
Sean Christians <security@seanchristians.com>
2025-12-13 18:18:51 -0500

    use aws-managed kms key

--- infrastructure/backend/s3.tf
+++ infrastructure/backend/s3.tf
@@ -25,7 +25,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
   bucket = aws_s3_bucket.terraform_state.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      sse_algorithm = "AES256"
     }
+    bucket_key_enabled       = true
+    blocked_encryption_types = ["SSE-C"]
   }
 }
