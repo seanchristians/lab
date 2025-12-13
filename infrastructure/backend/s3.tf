23fed3ba579da7367733524fa19056edd6ac2fed
Sean Christians <security@seanchristians.com>
2025-12-13 17:35:47 -0500

    add server side encrpyiton for state bucket

--- infrastructure/backend/s3.tf
+++ infrastructure/backend/s3.tf
@@ -20,3 +20,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
     }
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
