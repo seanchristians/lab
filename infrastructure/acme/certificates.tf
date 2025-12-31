ca6944bcfb541b1fbfaf8c550f70c3235f46e78e
Sean Christians <security@seanchristians.com>
2025-12-31 15:56:03 -0500

    Separate cert provisioner

--- infrastructure/acme/certificates.tf
+++ infrastructure/acme/certificates.tf
@@ -5,22 +5,39 @@ resource "acme_certificate" "veronica" {
   dns_challenge {
     provider = "porkbun"
   }
+}
+
+# Spearating the resource from the provisioner allows me to use Terraform's
+# "replace" method when I need to re-run the provisioner without having to
+# fully re-create the resource
+
+locals {
+  manual_cert_redeploy_token = {
+    veronica = "0BA76587-7E57-4E7B-85AF-90B43E839502"
+  }
+}
+
+resource "terraform_data" "certificate_provisioner_veronica" {
+  triggers_replace = [
+    acme_certificate.veronica.certificate_serial,
+    local.manual_cert_redeploy_token.veronica
+  ]
 
   connection {
     type            = "ssh"
     user            = "certmgr"
-    host            = self.certificate_domain
+    host            = acme_certificate.veronica.certificate_domain
     target_platform = "unix"
   }
 
   provisioner "file" {
-    content     = self.private_key_pem
-    destination = "/home/certmgr/ssl/${self.certificate_domain}.key"
+    content     = acme_certificate.veronica.private_key_pem
+    destination = "/etc/ssl/${acme_certificate.veronica.certificate_domain}/key.pem"
   }
 
   provisioner "file" {
-    content     = "${self.certificate_pem}${self.issuer_pem}"
-    destination = "/home/certmgr/ssl/${self.certificate_domain}.fullchain.cer"
+    content     = "${acme_certificate.veronica.certificate_pem}${acme_certificate.veronica.issuer_pem}"
+    destination = "/etc/ssl/${acme_certificate.veronica.certificate_domain}/fullchain.cer"
   }
 
   provisioner "remote-exec" {
