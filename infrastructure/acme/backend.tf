c01012d2f5a5ec6b3bc5f321159d9885bdd36494
Sean Christians <security@seanchristians.com>
2026-02-01 18:58:16 -0500

    Remove backend file
    This is automatically provided by CI

--- infrastructure/acme/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "seanchristians-lab-terraform-state"
-    key          = "acme/terraform.tfstate"
-    region       = "ca-central-1"
-    use_lockfile = true
-  }
-}
