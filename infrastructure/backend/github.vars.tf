e588140e74213dcfe0714b51c21bf17657713787
Sean Christians <security@seanchristians.com>
2026-02-01 21:56:00 -0500

    aws region

--- infrastructure/backend/github.vars.tf
+++ infrastructure/backend/github.vars.tf
@@ -2,6 +2,12 @@ locals {
   github_repo = "lab"
 }
 
+resource "github_actions_variable" "backend_aws_region" {
+  repository    = local.github_repo
+  variable_name = "TF_BACKEND_AWS_REGION"
+  value         = aws_s3_bucket.terraform_state.bucket_region
+}
+
 resource "github_actions_variable" "backend_aws_role" {
   repository    = local.github_repo
   variable_name = "TF_BACKEND_AWS_ROLE"
