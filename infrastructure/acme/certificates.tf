2071dd13753cbda95c39598066c222c1e56154cb
Sean Christians <security@seanchristians.com>
2026-02-01 19:12:28 -0500

    provide auth via SOPS

--- infrastructure/acme/certificates.tf
+++ infrastructure/acme/certificates.tf
@@ -1,9 +1,15 @@
+data "sops_file" "dns_providers" {
+  source_file = "dns-providers.enc.json"
+}
+
 resource "acme_certificate" "veronica" {
   account_key_pem = acme_registration.letsencrypt.account_key_pem
   common_name     = "veronica.${var.domain}"
 
   dns_challenge {
-    provider = "porkbun"
+    provider               = "porkbun"
+    PORKBUN_API_KEY        = data.sops_file.dns_providers.data["PORKBUN_API_KEY"]
+    PORKBUN_SECRET_API_KEY = data.sops_file.dns_providers.data["PORKBUN_SECRET_API_KEY"]
   }
 }
 
