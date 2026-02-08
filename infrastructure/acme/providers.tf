41a12d25f1ef12ba60fc01481461f9341cc69f43
Sean Christians <sean.christians@vention.cc>
2026-02-08 14:13:05 -0500

    Remove sops provider

--- infrastructure/acme/providers.tf
+++ infrastructure/acme/providers.tf
@@ -1,17 +1,11 @@
 terraform {
   required_providers {
-    sops = {
-      source  = "carlpett/sops"
-      version = "1.3.0"
-    }
     acme = {
       source  = "vancluever/acme"
       version = "2.43.0"
     }
   }
 }
-
-provider "sops" {}
 
 provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
