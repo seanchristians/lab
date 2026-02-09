16280abb60b1c2ca23c4a00505759a9ef91bf589
Sean Christians <sean.christians@vention.cc>
2026-02-08 23:02:00 -0500

    set depends_on in tailscale_device data source

--- infrastructure/network/venus.dns.tf
+++ infrastructure/network/venus.dns.tf
@@ -1,9 +1,5 @@
 data "tailscale_device" "venus" {
   hostname = split(".", aws_instance.venus.private_dns)[0]
-}
-
-resource "terraform_data" "tailscale_device_venus" {
-  input = data.tailscale_device.venus.name
 
   depends_on = [tailscale_dns_preferences.default]
 }
@@ -12,5 +8,5 @@ resource "porkbun_dns_record" "venus" {
   domain    = data.porkbun_domain.network.domain
   subdomain = "venus"
   type      = "CNAME"
-  content   = terraform_data.tailscale_device_venus.output
+  content   = data.tailscale_device.venus.name
 }
--- infrastructure/network/veronica.dns.tf
+++ infrastructure/network/veronica.dns.tf
@@ -1,9 +1,5 @@
 data "tailscale_device" "veronica" {
   hostname = "veronica"
-}
-
-resource "terraform_data" "tailscale_device_veronica" {
-  input = data.tailscale_device.veronica.name
 
   depends_on = [tailscale_dns_preferences.default]
 }
@@ -12,5 +8,5 @@ resource "porkbun_dns_record" "veronica" {
   domain    = data.porkbun_domain.network.domain
   subdomain = "veronica"
   type      = "CNAME"
-  content   = terraform_data.tailscale_device_veronica.output
+  content   = data.tailscale_device.veronica.name
 }
