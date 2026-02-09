63ef225719bb50536d812e24556d8a11809cc5d2
Sean Christians <sean.christians@vention.cc>
2026-02-08 22:04:14 -0500

    enable instance monitoring

--- infrastructure/network/venus.ec2.tf
+++ infrastructure/network/venus.ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "venus" {
   ami                 = "ami-0c096d78166072826"
   instance_type       = "t4g.nano"
   enable_primary_ipv6 = true
+  monitoring          = true
 
   vpc_security_group_ids = [
     aws_security_group.venus.id
