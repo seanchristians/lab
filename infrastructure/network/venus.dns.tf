f9bb25936f2c71292e4fe2e55fa9213ec7f83762
Sean Christians <sean.christians@vention.cc>
2026-02-08 22:54:31 -0500

    Use terraform_data to wait for tailscale DNS

--- infrastructure/network/venus.dns.tf
+++ infrastructure/network/venus.dns.tf
@@ -2,11 +2,15 @@ data "tailscale_device" "venus" {
   hostname = split(".", aws_instance.venus.private_dns)[0]
 }
 
+resource "terraform_data" "tailscale_device_venus" {
+  input = data.tailscale_device.venus.name
+
+  depends_on = [tailscale_dns_preferences.default]
+}
+
 resource "porkbun_dns_record" "venus" {
   domain    = data.porkbun_domain.network.domain
   subdomain = "venus"
   type      = "CNAME"
-  content   = data.tailscale_device.venus.name
-
-  depends_on = [tailscale_dns_preferences.default]
+  content   = terraform_data.tailscale_device_venus.output
 }
--- infrastructure/network/veronica.dns.tf
+++ infrastructure/network/veronica.dns.tf
@@ -2,11 +2,15 @@ data "tailscale_device" "veronica" {
   hostname = "veronica"
 }
 
+resource "terraform_data" "tailscale_device_veronica" {
+  input = data.tailscale_device.veronica.name
+
+  depends_on = [tailscale_dns_preferences.default]
+}
+
 resource "porkbun_dns_record" "veronica" {
   domain    = data.porkbun_domain.network.domain
   subdomain = "veronica"
   type      = "CNAME"
-  content   = data.tailscale_device.veronica.name
-
-  depends_on = [tailscale_dns_preferences.default]
+  content   = terraform_data.tailscale_device_veronica.output
 }
