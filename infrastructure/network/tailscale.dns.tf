8acd58f13ef43a37de24c3fcdbff657118b5b7f7
Sean Christians <sean.christians@vention.cc>
2026-02-08 22:16:51 -0500

    separate resources for tailscale dns settings

--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -1,4 +1,7 @@
-resource "tailscale_dns_configuration" "default" {
-  magic_dns    = true
-  search_paths = data.porkbun_domain.network
+resource "tailscale_dns_preferences" "default" {
+  magic_dns = true
+}
+
+resource "tailscale_dns_search_paths" "network" {
+  search_paths = [data.porkbun_domain.network]
 }
