5d65a1270d29fea4e3d495add7fcd27cb67153a1
Sean Christians <sean.christians@vention.cc>
2026-02-08 21:56:09 -0500

    Remove redundant az reference in ec2

--- infrastructure/network/venus.ec2.tf
+++ infrastructure/network/venus.ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "venus" {
   ami                 = "ami-0c096d78166072826"
   instance_type       = "t4g.nano"
-  availability_zone   = data.aws_subnet.default.availability_zone
   enable_primary_ipv6 = true
 
   vpc_security_group_ids = [
