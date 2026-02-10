686f5cc5b66fdc2143bc78bd71211e4513a3e154
Sean Christians <sean.christians@vention.cc>
2026-02-09 23:40:48 -0500

    Remove completed moved operation

--- infrastructure/network/venus.sg.tf
+++ infrastructure/network/venus.sg.tf
@@ -8,8 +8,3 @@ resource "aws_vpc_security_group_egress_rule" "venus_allow_internet_v6" {
   cidr_ipv6   = "::/0"
   ip_protocol = -1
 }
-
-moved {
-  from = aws_vpc_security_group_egress_rule.venus_all_all_v6
-  to   = aws_vpc_security_group_egress_rule.venus_allow_internet_v6
-}
