46689c16b2c88917f104ed244ccd6a2d386be05f
Sean Christians <sean.christians@vention.cc>
2026-02-09 23:40:22 -0500

    remove search paths and disable magic dns

--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -1,12 +1,3 @@
 resource "tailscale_dns_preferences" "default" {
-  magic_dns = true
-}
-
-resource "tailscale_dns_search_paths" "network" {
-  search_paths = [local.domain]
-}
-
-locals {
-  tailnet_dns_name = "tail18a6a8.ts.net"
-  domain           = "scchq.net"
+  magic_dns = false
 }
