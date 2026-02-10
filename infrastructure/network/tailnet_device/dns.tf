7901b66061686d9a880e8f917c52ce930505006c
Sean Christians <sean.christians@vention.cc>
2026-02-09 23:34:21 -0500

    create records for all device IPs

--- infrastructure/network/tailnet_device/dns.tf
+++ infrastructure/network/tailnet_device/dns.tf
@@ -6,14 +6,11 @@ data "tailscale_device" "this" {
   hostname = var.tailnet_hostname
 }
 
-resource "porkbun_dns_record" "ipv4" {
+resource "porkbun_dns_record" "this" {
+  for_each = toset(data.tailscale_device.this.addresses)
+
   domain    = local.domain
   subdomain = var.subdomain
-  type      = "A"
-  content   = data.tailscale_device.this.addresses
-}
-
-moved {
-  from = porkbun_dns_record.this
-  to   = porkbun_dns_record.ipv4
+  type      = can(cidrnetmask("${each.key}/32")) ? "A" : "AAAA"
+  content   = each.key
 }
