3ee429844a0b3c6054296d11e5bfc41d6a2d7aad
Sean Christians <sean.christians@vention.cc>
2026-02-09 22:20:10 -0500

    Remove temp code

--- infrastructure/network/veronica.dns.tf
+++ infrastructure/network/veronica.dns.tf
@@ -1,14 +1,3 @@
-# data "tailscale_device" "veronica" {
-#   hostname = "veronica"
-# }
-
-# resource "porkbun_dns_record" "veronica" {
-#   domain    = data.porkbun_domain.network.domain
-#   subdomain = "veronica"
-#   type      = "CNAME"
-#   content   = "${data.tailscale_device.veronica.hostname}.${local.tailnet_dns_name}"
-# }
-
 module "dns_veronica" {
   source           = "./tailnet_device"
   subdomain        = "veronica"
