d13d5a90b2f9cb883a11c07c53fe6868e0ac8c9f
Sean Christians <sean.christians@vention.cc>
2026-02-10 08:17:44 -0500

    tailscale list of tagged IPs

--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -1,3 +1,26 @@
 resource "tailscale_dns_preferences" "default" {
   magic_dns = false
 }
+
+data "tailscale_devices" "tailnet" {}
+
+locals {
+  tailnet_tagged_ips = flatten([
+    for device in data.tailscale_devices.tailnet.devices : [
+      for address in device.addresses : {
+        device  = device.hostname
+        ip      = address
+        is_ipv4 = can(cidrnetmask("${address}/32"))
+      } if length(device.tags) > 0
+    ]
+  ])
+}
+
+# resource "porkbun_dns_record" "tailnet" {
+#   for_each = []
+
+#   domain    = local.domain
+#   subdomain = var.subdomain
+#   type      = can(cidrnetmask("${each.key}/32")) ? "A" : "AAAA"
+#   content   = each.key
+# }
