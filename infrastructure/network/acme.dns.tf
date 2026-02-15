0e5e6ce334e5ada8d57780c41b9f27a5ad35f462
Sean Christians <sean.christians@vention.cc>
2026-02-15 17:10:46 -0500

    Create DNS zones in GCore for ACME challenge-enabled subdomains

--- infrastructure/network/acme.dns.tf
+++ infrastructure/network/acme.dns.tf
@@ -3,6 +3,10 @@ locals {
     "ns1.gcorelabs.net",
     "ns2.gcdn.services"
   ]
+
+  acme_challenge_domains = [
+    "veronica"
+  ]
 }
 
 resource "porkbun_nameservers" "acme_challenge" {
@@ -10,9 +14,20 @@ resource "porkbun_nameservers" "acme_challenge" {
   nameservers = local.gcore_nameservers
 }
 
-resource "gcore_dns_zone" "veronica" {
-  name   = "veronica.${data.porkbun_domain.acme_challenge.domain}"
+resource "gcore_dns_zone" "acme_challenge" {
+  for_each = toset(local.acme_challenge_domains)
+
+  name   = "${each.key}.${data.porkbun_domain.acme_challenge.domain}"
   dnssec = true
 
   depends_on = [porkbun_nameservers.acme_challenge]
+}
+
+resource "porkbun_dns_record" "acme_challenge" {
+  for_each = toset(local.acme_challenge_domains)
+
+  domain    = data.porkbun_domain.acme_challenge.domain
+  subdomain = "_acme-challenge.${each.key}"
+  type      = "CNAME"
+  content   = gcore_dns_zone.acme_challenge[each.key].name
 }
