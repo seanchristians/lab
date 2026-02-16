83f06343f7a6832d099c6596f270971bd5f6d21e
Sean Christians <sean.christians@vention.cc>
2026-02-15 20:54:38 -0500

    Remove deSEC auto token generator

--- infrastructure/network/acme.dns.tf
+++ infrastructure/network/acme.dns.tf
@@ -28,31 +28,3 @@ resource "porkbun_dns_record" "acme_challenge" {
   type      = "CNAME"
   content   = each.value.name
 }
-
-resource "desec_token" "acme_challenge" {
-  for_each = desec_domain.acme_challenge
-
-  auto_policy        = true
-  perm_create_domain = false
-  perm_delete_domain = false
-  perm_manage_tokens = false
-}
-
-resource "desec_token_policy" "acme_challenge" {
-  for_each = desec_token.acme_challenge
-
-  token_id   = each.value.id
-  perm_write = true
-  domain     = desec_domain.acme_challenge[each.key].name
-  type       = "TXT"
-}
-
-resource "terraform_data" "desec_tokens" {
-  for_each = desec_token.acme_challenge
-
-  input = each.value.token
-
-  lifecycle {
-    ignore_changes = [input]
-  }
-}
