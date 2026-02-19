4bdb34d83e34191f6a36491d690ba22fb0eba90c
Sean Christians <sean.christians@vention.cc>
2026-02-18 22:50:33 -0500

    reimplement domains

--- infrastructure/network/acme.veronica.tf
+++ infrastructure/network/acme.veronica.tf
@@ -1,26 +1,26 @@
-resource "desec_token" "veronica" {
-  name               = "veronica"
-  auto_policy        = true
-  perm_create_domain = false
-  perm_delete_domain = false
-  perm_manage_tokens = false
+# resource "desec_token" "veronica" {
+#   name               = "veronica"
+#   auto_policy        = true
+#   perm_create_domain = false
+#   perm_delete_domain = false
+#   perm_manage_tokens = false
 
-  connection {
-    type            = "ssh"
-    user            = "acme"
-    host            = "veronica.${data.porkbun_domain.network.domain}"
-    target_platform = "unix"
-  }
+#   connection {
+#     type            = "ssh"
+#     user            = "acme"
+#     host            = "veronica.${data.porkbun_domain.network.domain}"
+#     target_platform = "unix"
+#   }
 
-  provisioner "file" {
-    content     = self.token
-    destination = "/etc/acme/auth/desec.token"
-  }
-}
+#   provisioner "file" {
+#     content     = self.token
+#     destination = "/etc/acme/auth/desec.token"
+#   }
+# }
 
-resource "desec_token_policy" "veronica" {
-  token_id   = desec_token.veronica.id
-  perm_write = true
-  domain     = desec_domain.acme_challenge["veronica"].name
-  type       = "TXT"
-}
+# resource "desec_token_policy" "veronica" {
+#   token_id   = desec_token.veronica.id
+#   perm_write = true
+#   domain     = desec_domain.acme_challenge["veronica"].name
+#   type       = "TXT"
+# }
