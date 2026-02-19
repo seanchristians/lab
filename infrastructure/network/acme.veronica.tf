f1d5b4cc7379946c2a570894c45dfb59019f2063
Sean Christians <sean.christians@vention.cc>
2026-02-18 23:47:47 -0500

    ignore server changes and add debug to domain resource

--- infrastructure/network/acme.veronica.tf
+++ infrastructure/network/acme.veronica.tf
@@ -1,6 +1,7 @@
 resource "restapi_object" "desec_token_veronica" {
-  provider = restapi.desec
-  path     = "/auth/tokens"
+  provider                = restapi.desec
+  path                    = "/auth/tokens"
+  ignore_server_additions = true
 
   data = jsonencode({
     name               = "veronica"
@@ -11,23 +12,27 @@ resource "restapi_object" "desec_token_veronica" {
 }
 
 resource "restapi_object" "desec_token_default_policy_veronica" {
-  provider = restapi.desec
-  path     = "/auth/tokens/${restapi_object.desec_token_veronica.id}/policies/rrsets"
+  provider                = restapi.desec
+  path                    = "/auth/tokens/${restapi_object.desec_token_veronica.id}/policies/rrsets"
+  ignore_server_additions = true
+
 
   data = jsonencode({
-    domain  = null
-    subname = null
-    type    = null
+    domain     = null
+    subname    = null
+    type       = null
+    perm_write = false
   })
 }
 
 resource "restapi_object" "desec_token_policy_veronica" {
-  provider = restapi.desec
-  path     = "/auth/tokens/${restapi_object.desec_token_veronica.id}/policies/rrsets"
+  provider                = restapi.desec
+  path                    = "/auth/tokens/${restapi_object.desec_token_veronica.id}/policies/rrsets"
+  ignore_server_additions = true
 
   data = jsonencode({
-    domain     = restapi_object.desec_domain_acme_challenge["veronica"].api_data.name
-    subname    = null
+    domain     = data.porkbun_domain.acme_challenge.domain
+    subname    = "veronica"
     type       = "TXT"
     perm_write = true
   })
