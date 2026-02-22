58e3199982fc067c43aa7172e3f1ab28c880963e
Sean Christians <sean.christians@vention.cc>
2026-02-22 18:06:01 -0500

    Simplify ip resolution logic

--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -3,31 +3,22 @@ resource "tailscale_dns_preferences" "default" {
 }
 
 resource "porkbun_dns_record" "tailnet" {
-  for_each = tomap({ for ipObj in local.tailnet_tagged_ips : ipObj.ip => ipObj })
+  for_each = local.tailnet_ips
 
-  domain    = data.porkbun_domain.network.domain
+  domain    = local.network_domain
   subdomain = each.value.name
-  type      = each.value.is_ipv4 ? "A" : "AAAA"
+  type      = can(cidrnetmask("${each.key}/32")) ? "A" : "AAAA"
   content   = each.key
 }
 
 locals {
   tailnet_tags           = keys(jsondecode(data.local_file.tailnet_policy.content).tagOwners)
-  tailnet_tagged_devices = flatten([for devices in data.tailscale_devices.tagged_device : devices.devices])
-
-  tailnet_tagged_ips = flatten([
-    for device in local.tailnet_tagged_devices : [
-      for address in device.addresses : {
-        ip      = address
-        is_ipv4 = can(cidrnetmask("${address}/32"))
-        name    = join(".", slice(split(".", device.name), 0, length(split(".", device.name)) - 3))
-      }
-    ]
-  ])
+  tailnet_tagged_devices = toset(flatten(data.tailscale_devices.tagged_devices[*].devices))
+  tailnet_ips            = merge([for device in local.tailnet_tagged_devices : { for address in device.addresses : address => device }]...)
 }
 
-data "tailscale_devices" "tagged_device" {
-  for_each = toset(local.tailnet_tags)
+data "tailscale_devices" "tagged_devices" {
+  for_each = local.tailnet_tags
 
   filter {
     name   = "isEphemeral"
