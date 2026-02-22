0bfa86f0e663ece40047f6199c12ba4ffdec2c18
Sean Christians <sean.christians@vention.cc>
2026-02-22 18:26:18 -0500

    Switch splat to for expression for tagged_devices objects

--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -13,7 +13,7 @@ resource "porkbun_dns_record" "tailnet" {
 
 locals {
   tailnet_tags           = toset(keys(jsondecode(data.local_file.tailnet_policy.content).tagOwners))
-  tailnet_tagged_devices = toset(flatten(data.tailscale_devices.tagged_devices[*].devices))
+  tailnet_tagged_devices = distinct(flatten([for tagged_devices in data.tailscale_devices.tagged_devices : tagged_devices.devices]))
   tailnet_ips            = merge([for device in local.tailnet_tagged_devices : { for address in device.addresses : address => device }]...)
 }
 
