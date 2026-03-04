8c718a79228e153ed6db1bbf1fb82fc1e1dce964
Sean Christians <sean.christians@vention.cc>
2026-03-03 21:19:18 -0500

    identify nameservers from DNS

--- infrastructure/network/acme.dns.tf
+++ infrastructure/network/acme.dns.tf
@@ -1,13 +1,10 @@
-locals {
-  desec_nameservers = [
-    "ns1.desec.io",
-    "ns2.desec.org"
-  ]
+data "dns_ns_record_set" "desec" {
+  host = "desec.io"
 }
 
 resource "porkbun_nameservers" "acme_challenge" {
   domain      = local.acme_challenge_domain
-  nameservers = local.desec_nameservers
+  nameservers = data.dns_ns_record_set.desec.nameservers
 }
 
 resource "restapi_object" "desec_domain_acme_challenge" {
