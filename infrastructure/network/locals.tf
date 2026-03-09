ff66373601087cfad6287dbb11c6d370437d6f99
Sean Christians <sean.christians@vention.cc>
2026-03-09 02:01:30 -0400

    add preferred dns servers

--- infrastructure/network/locals.tf
+++ infrastructure/network/locals.tf
@@ -8,6 +8,11 @@ locals {
 
   network_domain        = local.porkbun_active_domain["scchq.net"].domain
   acme_challenge_domain = local.porkbun_active_domain["sean.directory"].domain
+
+  preferred_public_dns_servers = {
+    google     = ["8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844"]
+    cloudflare = ["1.1.1.1", "1.0.0.1", "2606:4700:4700::1111", "2606:4700:4700::1001"]
+  }
 }
 
 data "external" "module_path_in_git_repo" {
--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -1,5 +1,13 @@
-resource "tailscale_dns_preferences" "default" {
-  magic_dns = true
+resource "tailscale_dns_configuration" "default" {
+  magic_dns          = true
+  override_local_dns = true
+
+  dynamic "nameservers" {
+    for_each = [flatten(values(local.preferred_public_dns_servers))]
+    content {
+      address = each.key
+    }
+  }
 }
 
 resource "porkbun_dns_record" "tailnet" {
