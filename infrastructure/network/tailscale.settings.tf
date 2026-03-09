0b9624d1b9c08004bd756721d2164637466925ec
Sean Christians <sean.christians@vention.cc>
2026-03-09 02:11:41 -0400

    rename tailnet settings to 'default'

--- infrastructure/network/tailscale.settings.tf
+++ infrastructure/network/tailscale.settings.tf
@@ -1,4 +1,4 @@
-resource "tailscale_tailnet_settings" "primary" {
+resource "tailscale_tailnet_settings" "default" {
   acls_externally_managed_on = true
   acls_external_link         = "https://github.com/seanchristians/lab/blob/main/${data.external.module_path_in_git_repo.result.content}${data.local_file.tailnet_policy.filename}"
 
@@ -9,4 +9,9 @@ resource "tailscale_tailnet_settings" "primary" {
   users_approval_on = true
 
   https_enabled = true
+}
+
+moved {
+  from = tailscale_tailnet_settings.primary
+  to   = tailscale_tailnet_settings.default
 }
