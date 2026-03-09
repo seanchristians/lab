00368ba0803282fe44d8578d769e59fdbf6e6840
Sean Christians <sean.christians@vention.cc>
2026-03-09 02:09:18 -0400

    remove for_each from tuple

--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -3,7 +3,7 @@ resource "tailscale_dns_configuration" "default" {
   override_local_dns = true
 
   dynamic "nameservers" {
-    for_each = [flatten(values(local.preferred_public_dns_servers))]
+    for_each = flatten(values(local.preferred_public_dns_servers))
     content {
       address = nameservers.value
     }
