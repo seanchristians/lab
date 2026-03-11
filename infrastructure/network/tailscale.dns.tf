eaf6db78194c12cc4877eb98967bba692b89b269
Sean Christians <sean.christians@vention.cc>
2026-03-10 23:51:22 -0400

    Remove Porkbun DNS records for Tailnet devices

--- infrastructure/network/tailscale.dns.tf
+++ infrastructure/network/tailscale.dns.tf
@@ -9,32 +9,3 @@ resource "tailscale_dns_configuration" "default" {
     }
   }
 }
-
-resource "porkbun_dns_record" "tailnet" {
-  for_each = local.tailnet_ips
-
-  domain    = local.network_domain
-  subdomain = regex("^(.+)\\.[^.]+\\.ts\\.net$", each.value.name)[0]
-  type      = can(cidrnetmask("${each.key}/32")) ? "A" : "AAAA"
-  content   = each.key
-}
-
-locals {
-  tailnet_tags           = toset(keys(jsondecode(data.local_file.tailnet_policy.content).tagOwners))
-  tailnet_tagged_devices = distinct(flatten([for tagged_devices in data.tailscale_devices.tagged_devices : tagged_devices.devices]))
-  tailnet_ips            = merge([for device in local.tailnet_tagged_devices : { for address in device.addresses : address => device }]...)
-}
-
-data "tailscale_devices" "tagged_devices" {
-  for_each = local.tailnet_tags
-
-  filter {
-    name   = "isEphemeral"
-    values = ["false"]
-  }
-
-  filter {
-    name   = "tags"
-    values = [each.key]
-  }
-}
