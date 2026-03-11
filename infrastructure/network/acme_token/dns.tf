4d616f70f519ed8a57e33f4b7355ad45704912c9
Sean Christians <sean.christians@vention.cc>
2026-03-10 23:48:56 -0400

    Remove ACME challenge code

--- infrastructure/network/acme_token/dns.tf
+++
@@ -1,6 +0,0 @@
-resource "porkbun_dns_record" "this" {
-  domain    = var.domain
-  subdomain = "_acme-challenge.${var.subdomain}"
-  type      = "CNAME"
-  content   = "${var.subdomain}.${var.acme_domain}"
-}
