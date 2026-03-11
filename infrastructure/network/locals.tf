a5e9c63c857fb7cf55c394a7b30b7a1e3047027d
Sean Christians <sean.christians@vention.cc>
2026-03-11 00:33:59 -0400

    Remove unused prokbun domains

--- infrastructure/network/locals.tf
+++ infrastructure/network/locals.tf
@@ -1,14 +1,4 @@
-data "porkbun_domains" "all" {}
-
 locals {
-  porkbun_active_domain = {
-    for domain in data.porkbun_domains.all.domains : domain.domain => domain
-    if domain.status == "ACTIVE"
-  }
-
-  network_domain        = local.porkbun_active_domain["scchq.net"].domain
-  acme_challenge_domain = local.porkbun_active_domain["sean.directory"].domain
-
   preferred_public_dns_servers = {
     google     = ["8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844"]
     cloudflare = ["1.1.1.1", "1.0.0.1", "2606:4700:4700::1111", "2606:4700:4700::1001"]
