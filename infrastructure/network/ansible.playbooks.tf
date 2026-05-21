c55a392e4d15b2724f15607bf208361e0a6b43a7
Sean Christians <sean.christians@vention.cc>
2026-05-21 11:38:31 -0400

    Publish the admin port for the wg-easy service (#181)

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -1,4 +1,5 @@
 resource "ansible_playbook" "wg_easy_podman" {
-  name     = data.tailscale_device.squiggle_darkened.name
-  playbook = "ansible-playbooks/wg-easy.yaml"
+  name       = data.tailscale_device.squiggle_darkened.name
+  playbook   = "ansible-playbooks/wg-easy.yaml"
+  replayable = false
 }
