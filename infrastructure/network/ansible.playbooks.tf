22769ee87618c4025def45d5a494baea9900ebf9
Sean Christians <sean.christians@vention.cc>
2026-05-21 09:20:03 -0400

    follow standard terraform naming convention

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -1,4 +1,4 @@
 resource "ansible_playbook" "wg_easy_podman" {
-  name     = data.tailscale_device.squiggle-darkened.name
+  name     = data.tailscale_device.squiggle_darkened.name
   playbook = "ansible-playbooks/wg-easy.yaml"
 }
--- infrastructure/network/tailscale.devices.tf
+++ infrastructure/network/tailscale.devices.tf
@@ -1,3 +1,3 @@
-data "tailscale_device" "squiggle-darkened" {
+data "tailscale_device" "squiggle_darkened" {
   hostname = "squiggle-darkened"
 }
