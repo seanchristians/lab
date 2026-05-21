0a21fb000ee67f3da36b1811b74540e898710396
Sean Christians <sean.christians@vention.cc>
2026-05-21 17:25:31 -0400

    Dynamically re-run Ansible playbooks with Terraform actions (#184)
    Run Ansible playbook with a Terraform action triggered by a
    `terraform_data` block, this way we only re-run the action when it's
    configuration file is updated.
    
    Also defines the Ansible inventory based on a lists of device hostnames.

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -1,5 +1,49 @@
-resource "ansible_playbook" "wg_easy_podman" {
-  name       = data.tailscale_device.squiggle_darkened.name
-  playbook   = "ansible-playbooks/wg-easy.yaml"
-  replayable = false
+action "ansible_playbook_run" "wg_easy_podman" {
+  config {
+    playbooks   = ["ansible-playbooks/wg-easy.yaml"]
+    inventories = [data.ansible_inventory.primary.json]
+    user        = "root"
+  }
 }
+
+resource "terraform_data" "wireguard_server" {
+  triggers_replace = [
+    data.local_file.playbook_wg_easy_podman,
+    data.ansible_inventory.primary
+  ]
+
+  lifecycle {
+    action_trigger {
+      events  = [after_create, after_update]
+      actions = [action.ansible_playbook_run.wg_easy_podman]
+    }
+  }
+}
+
+locals {
+  wireguard_server_hostnames = [
+    "squiggle-darkened"
+  ]
+}
+
+data "local_file" "playbook_wg_easy_podman" {
+  filename = "ansible-playbooks/wg-easy.yaml"
+}
+
+data "ansible_inventory" "primary" {
+  group {
+    name = "wireguard_servers"
+
+    dynamic "host" {
+      for_each = toset([
+        for device in data.tailscale_devices.all.devices : device.name
+        if contains(local.wireguard_server_hostnames, device.hostname)
+      ])
+      content {
+        name = host.value
+      }
+    }
+  }
+}
+
+data "tailscale_devices" "all" {}
--- infrastructure/network/tailscale.devices.tf
+++
@@ -1,3 +0,0 @@
-data "tailscale_device" "squiggle_darkened" {
-  hostname = "squiggle-darkened"
-}
