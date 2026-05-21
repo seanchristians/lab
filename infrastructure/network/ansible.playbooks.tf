434b45a554280e80f99bea8bbd49d9f36579497d
Sean Christians <sean.christians@vention.cc>
2026-05-21 17:01:59 -0400

    Use action and terraform_data to dynamically re-run playbooks

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -1,5 +1,49 @@
-resource "ansible_playbook" "wg_easy_podman" {
-  name       = data.tailscale_device.squiggle_darkened.name
-  playbook   = "ansible-playbooks/wg-easy.yaml"
-  replayable = false
+action "ansible_playbook_run" "wg_easy_podman" {
+  config {
+    playbooks   = [data.local_file.playbook_wg_easy_podman.filename]
+    inventories = [data.ansible_inventory.minecraft_servers.json]
+  }
+}
+
+resource "terraform_data" "wireguard_server" {
+  triggers_replace = [
+    data.local_file.playbook_wg_easy_podman,
+    data.ansible_inventory.wireguard_servers
+  ]
+
+  lifecycle {
+    action_trigger {
+      events  = [after_create, after_update]
+      actions = [action.ansible_playbook_run.wg_easy_podman]
+    }
+  }
+}
+
+locals {
+  wireguard_server_hostnames = [
+    "squiggle-darkened"
+  ]
+
+  tailscale_wireguard_servers = [
+    for device in data.tailscale_devices.all : device if contains(local.wireguard_server_hostnames, device.hostname)
+  ]
+}
+
+data "tailscale_devices" "all" {}
+
+data "local_file" "playbook_wg_easy_podman" {
+  filename = "ansible-playbooks/wg-easy.yaml"
+}
+
+data "ansible_inventory" "primary" {
+  group {
+    name = "wireguard_servers"
+
+    dynamic "host" {
+      for_each = toset([for device in local.tailscale_wireguard_servers : device.name])
+      content {
+        name = host.value
+      }
+    }
+  }
 }
