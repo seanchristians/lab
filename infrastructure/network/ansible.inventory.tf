8e0c5951b19dbc6d54675115c34add62e69518f2
Sean Christians <sean.christians@vention.cc>
2026-05-22 14:48:33 -0400

    escape ipv6 in square brackets

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -2,7 +2,12 @@ resource "ansible_host" "squiggle_darkened" {
   name   = data.tailscale_device.squiggle_darkened.name
   groups = ["wireguard_servers"]
   variables = {
-    wireguard_admin_ports = [for ip in data.tailscale_device.squiggle_darkened.addresses : "${ip}:51821:51821/tcp"]
+    wireguard_admin_ports = concat(
+      [for ip in data.tailscale_device.squiggle_darkened.addresses : "${ip}:51821:51821/tcp"
+      if can(cidrnetmask(ip))], # ipv4
+      [for ip in data.tailscale_device.squiggle_darkened.addresses : "[${ip}]:51821:51821/tcp"
+      if !can(cidrnetmask(ip))] # escape ipv6 in []
+    )
   }
 }
 
