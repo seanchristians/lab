a8159458085dd390cdcaf15901ed245a0d7660a9
Sean Christians <sean.christians@vention.cc>
2026-05-22 14:35:08 -0400

    Run playbook with new ansible host resource

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -1,49 +1,16 @@
-action "ansible_playbook_run" "wg_easy_podman" {
-  config {
-    playbooks   = ["ansible-playbooks/wg-easy.yaml"]
-    inventories = [data.ansible_inventory.primary.json]
-    user        = "root"
-  }
-}
-
-resource "terraform_data" "wireguard_server" {
-  triggers_replace = [
-    data.local_file.playbook_wg_easy_podman,
-    data.ansible_inventory.primary
-  ]
+resource "ansible_playbook" "wg_easy_podman" {
+  name       = ansible_host.squiggle_darkened.name
+  playbook   = "ansible-playbooks/wg-easy.yaml"
+  replayable = false
 
   lifecycle {
-    action_trigger {
-      events  = [after_create, after_update]
-      actions = [action.ansible_playbook_run.wg_easy_podman]
-    }
+    replace_triggered_by = [
+      data.local_file.playbook_wg_easy_podman,
+      aansible_host.squiggle_darkened
+    ]
   }
-}
-
-locals {
-  wireguard_server_hostnames = [
-    "squiggle-darkened"
-  ]
 }
 
 data "local_file" "playbook_wg_easy_podman" {
   filename = "ansible-playbooks/wg-easy.yaml"
 }
-
-data "ansible_inventory" "primary" {
-  group {
-    name = "wireguard_servers"
-
-    dynamic "host" {
-      for_each = toset([
-        for device in data.tailscale_devices.all.devices : device.name
-        if contains(local.wireguard_server_hostnames, device.hostname)
-      ])
-      content {
-        name = host.value
-      }
-    }
-  }
-}
-
-data "tailscale_devices" "all" {}
