69988e5d1d436aaf6d440aa37afccc8c8825f585
Sean Christians <sean.christians@vention.cc>
2026-05-22 15:37:11 -0400

    Just set terraform_data based on file id

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -12,7 +12,7 @@ resource "ansible_playbook" "wg_easy_podman" {
 }
 
 resource "terraform_data" "playbook_wg_easy_podman" {
-  triggers_replace = data.local_file.playbook_wg_easy_podman
+  triggers_replace = data.local_file.playbook_wg_easy_podman.id
 }
 
 data "local_file" "playbook_wg_easy_podman" {
