db12d2851108d45d9078b71d378b5fd9ffd22d2c
Sean Christians <sean.christians@vention.cc>
2026-05-22 17:00:15 -0400

    add sentinel value back

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -6,7 +6,10 @@ action "ansible_playbook_run" "wg_easy_podman" {
 }
 
 resource "terraform_data" "playbook_wg_easy_podman" {
-  triggers_replace = data.local_file.playbook_wg_easy_podman.id
+  triggers_replace = [
+    data.local_file.playbook_wg_easy_podman.id,
+    "ECF964FD-0829-41F1-8F6B-703960297624"
+  ]
 
   lifecycle {
     replace_triggered_by = [ansible_group.wireguard_servers]
