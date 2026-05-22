af25a44bfe9a2524d022aae767b22eb39aa97585
Sean Christians <sean.christians@vention.cc>
2026-05-22 16:35:02 -0400

    Remove sentinel value from terraform_data resource

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -3,6 +3,7 @@ resource "ansible_group" "wireguard_servers" {
   children = [ansible_host.squiggle_darkened.id]
 
   lifecycle {
+    replace_triggered_by = [terraform_data.playbook_wg_easy_podman]
     action_trigger {
       actions = [action.ansible_playbook_run.wg_easy_podman]
       events  = [after_create, after_update]
