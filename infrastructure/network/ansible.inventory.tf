2055195c93cdd6de82ed771e58bc3cf83e7da5b1
Sean Christians <sean.christians@vention.cc>
2026-05-22 15:41:15 -0400

    Define wireguard_servers group explicitly

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -1,6 +1,11 @@
+resource "ansible_group" "wireguard_servers" {
+  name     = "wireguard_servers"
+  children = [ansible_host.squiggle_darkened.name]
+}
+
 resource "ansible_host" "squiggle_darkened" {
-  name   = data.tailscale_device.squiggle_darkened.name
-  groups = ["wireguard_servers"]
+  name = data.tailscale_device.squiggle_darkened.name
+
   variables = {
     wireguard_admin_ports = concat(
       [for ip in data.tailscale_device.squiggle_darkened.addresses : "${ip}:51821:51821/tcp"
