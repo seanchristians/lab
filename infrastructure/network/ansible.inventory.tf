0a797123cee88ab212a95a381da564243aca2f8b
Sean Christians <sean.christians@vention.cc>
2026-05-22 21:49:16 -0400

    add wireguard admin ports

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -5,7 +5,13 @@ locals {
         ansible_host           = data.tailscale_device.squiggle_darkened.name
         ansible_user           = "root"
         ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
-        test_variable          = "foo"
+        wireguard_admin_ports = concat([
+          for ip in data.tailscale_device.squiggle_darkened.addresses : "${ip}:51821:51821/tcp"
+          if can(cidrnetmask("${ip}/32"))
+          ], [
+          for ip in data.tailscale_device.squiggle_darkened.addresses : "[${ip}]:51821:51821/tcp"
+          if !can(cidrnetmask("${ip}/32"))
+        ])
       }
     } }
   }
