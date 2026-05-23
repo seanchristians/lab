c5390825f9e27f9aa426bfd0262540808cb02354
Sean Christians <sean.christians@vention.cc>
2026-05-22 21:50:48 -0400

    Remove Ansible-native inventory configuration (#196)

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -1,18 +1,19 @@
-resource "ansible_group" "wireguard_servers" {
-  name     = "wireguard_servers"
-  children = [ansible_host.squiggle_darkened.id]
-}
-
-resource "ansible_host" "squiggle_darkened" {
-  name = data.tailscale_device.squiggle_darkened.name
-
-  variables = {
-    wireguard_admin_ports = concat(
-      [for ip in data.tailscale_device.squiggle_darkened.addresses : "${ip}:51821:51821/tcp"
-      if can(cidrnetmask("${ip}/32"))], # ipv4
-      [for ip in data.tailscale_device.squiggle_darkened.addresses : "[${ip}]:51821:51821/tcp"
-      if !can(cidrnetmask("${ip}/32"))] # escape ipv6 in []
-    )
+locals {
+  ansible_inventory = {
+    wireguard_servers = { hosts = {
+      "squiggle-darkened" = {
+        ansible_host           = data.tailscale_device.squiggle_darkened.name
+        ansible_user           = "root"
+        ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
+        wireguard_admin_ports = concat([
+          for ip in data.tailscale_device.squiggle_darkened.addresses : "${ip}:51821:51821/tcp"
+          if can(cidrnetmask("${ip}/32"))
+          ], [
+          for ip in data.tailscale_device.squiggle_darkened.addresses : "[${ip}]:51821:51821/tcp"
+          if !can(cidrnetmask("${ip}/32"))
+        ])
+      }
+    } }
   }
 }
 
--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -1,19 +1,19 @@
 action "ansible_playbook_run" "wg_easy_podman" {
   config {
-    playbooks       = [local.ansible_playbooks.wg_easy_podman]
-    inventory_files = ["ansible-inventory.yaml"]
+    playbooks   = [local.ansible_playbooks.wg_easy_podman]
+    inventories = [jsonencode(local.ansible_inventory)]
   }
 }
 
 resource "terraform_data" "playbook_wg_easy_podman" {
+  input = "ECF964FD-0829-41F1-8F6B-703960297624" # Sentinel value to manually trigger action
+
   triggers_replace = [
     data.local_file.playbook_wg_easy_podman.id,
-    "ECF964FD-0829-41F1-8F6B-703960297624"
+    local.ansible_inventory.wireguard_servers
   ]
 
   lifecycle {
-    replace_triggered_by = [ansible_group.wireguard_servers]
-
     action_trigger {
       actions = [action.ansible_playbook_run.wg_easy_podman]
       events  = [after_create, after_update]
