9e26232e82b5f17edf8a993daeec62fc9ef615bb
Sean Christians <sean.christians@vention.cc>
2026-05-22 23:31:41 -0400

    Fix podman containers (#199)

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -6,9 +6,8 @@ action "ansible_playbook_run" "wg_easy_podman" {
 }
 
 resource "terraform_data" "playbook_wg_easy_podman" {
-  input = "ECF964FD-0829-41F1-8F6B-703960297624" # Sentinel value to manually trigger action
-
-  triggers_replace = [
+  input = [
+    "ECF964FD-0829-41F1-8F6B-703960297624", # Sentinel value to manually trigger action
     data.local_file.playbook_wg_easy_podman.id,
     local.ansible_inventory.wireguard_servers
   ]
