7907a282a0b14187c61cd536575c238ffcb4b366
Sean Christians <sean.christians@vention.cc>
2026-05-22 21:22:01 -0400

    Replace references to Ansible inventory

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -1,19 +1,19 @@
 action "ansible_playbook_run" "wg_easy_podman" {
   config {
-    playbooks       = [local.ansible_playbooks.wg_easy_podman]
-    inventory_files = ["ansible-inventory.yaml"]
+    playbooks   = [local.ansible_playbooks.wg_easy_podman]
+    inventories = [data.ansible_inventory.primary.json]
   }
 }
 
 resource "terraform_data" "playbook_wg_easy_podman" {
+  input = "ECF964FD-0829-41F1-8F6B-703960297624" # Sentinel value to manually trigger action
+
   triggers_replace = [
     data.local_file.playbook_wg_easy_podman.id,
-    "ECF964FD-0829-41F1-8F6B-703960297624"
+    data.ansible_inventory.primary.json
   ]
 
   lifecycle {
-    replace_triggered_by = [ansible_group.wireguard_servers]
-
     action_trigger {
       actions = [action.ansible_playbook_run.wg_easy_podman]
       events  = [after_create, after_update]
