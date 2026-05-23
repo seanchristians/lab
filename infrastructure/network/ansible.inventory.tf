1a75bd9f483142277c670876484b11478ad74bf5
Sean Christians <sean.christians@vention.cc>
2026-05-23 04:22:59 -0400

    simplify ansible inventory management and add ddns group

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -1,19 +1,26 @@
 locals {
-  ansible_inventory = {
-    wireguard_servers = { hosts = {
-      "squiggle-darkened" = {
-        ansible_host           = data.tailscale_device.squiggle_darkened.name
-        ansible_user           = "root"
-        ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
-        tailscale_auth_key     = tailscale_tailnet_key.container.key
-        tailnet_service        = tailscale_service.minecraft.id
-        wireguard_domain       = "${porkbun_dns_record.minecraft_server.subdomain}.${porkbun_dns_record.minecraft_server.domain}"
-        wireguard_dns          = "149.112.121.10,149.112.122.10,2620:10A:80BB::10,2620:10A:80BC::10" # CIRA Canadian Shield
-        wireguard_ipv4_cidr    = "172.30.0.0/24"
-        wireguard_ipv6_cidr    = "fdef:aced::/64"
-        desec_token            = ephemeral.desec_token.minecraft_domain.token
-      }
-    } }
+  ansible_inventory = { for group, hosts in local.ansible_groups : group => {
+    for host in hosts : host => ansible_hosts[host]
+  } }
+
+  ansible_groups = {
+    wireguard_servers = ["squiggle-darkened"],
+    ddns              = ["squiggle-darkened"]
+  }
+
+  ansible_hosts = {
+    "squiggle-darkened" = {
+      ansible_host           = data.tailscale_device.squiggle_darkened.name
+      ansible_user           = "root"
+      ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
+      tailscale_auth_key     = tailscale_tailnet_key.container.key
+      tailnet_service        = tailscale_service.minecraft.id
+      wireguard_domain       = "${porkbun_dns_record.minecraft_server.subdomain}.${porkbun_dns_record.minecraft_server.domain}"
+      wireguard_dns          = "149.112.121.10,149.112.122.10,2620:10A:80BB::10,2620:10A:80BC::10" # CIRA Canadian Shield
+      wireguard_ipv4_cidr    = "172.30.0.0/24"
+      wireguard_ipv6_cidr    = "fdef:aced::/64"
+      desec_token            = ephemeral.desec_token.minecraft_domain.token
+    }
   }
 }
 
