4a6f3197978c275871184f93d1888c62b6e723b5
Sean Christians <sean.christians@vention.cc>
2026-05-22 21:19:55 -0400

    Replace ansible resources with inventory data

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -1,18 +1,12 @@
-resource "ansible_group" "wireguard_servers" {
-  name     = "wireguard_servers"
-  children = [ansible_host.squiggle_darkened.id]
-}
+data "ansible_inventory" "primary" {
+  group {
+    name = "wireguard_servers"
 
-resource "ansible_host" "squiggle_darkened" {
-  name = data.tailscale_device.squiggle_darkened.name
-
-  variables = {
-    wireguard_admin_ports = concat(
-      [for ip in data.tailscale_device.squiggle_darkened.addresses : "${ip}:51821:51821/tcp"
-      if can(cidrnetmask("${ip}/32"))], # ipv4
-      [for ip in data.tailscale_device.squiggle_darkened.addresses : "[${ip}]:51821:51821/tcp"
-      if !can(cidrnetmask("${ip}/32"))] # escape ipv6 in []
-    )
+    host {
+      name                   = data.tailscale_device.squiggle_darkened.name
+      ansible_user           = "root"
+      ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
+    }
   }
 }
 
