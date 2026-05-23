d8c55de83f31271c441157880dadd528bdbc540b
Sean Christians <sean.christians@vention.cc>
2026-05-23 03:39:03 -0400

    rename ddns_proxy

--- infrastructure/network/domains.tf
+++ infrastructure/network/domains.tf
@@ -2,12 +2,16 @@ resource "porkbun_dns_record" "minecraft_server" {
   domain    = "seanchristians.ca"
   subdomain = "minecraft"
   type      = "CNAME"
-  content   = "minecraft.sean.directory"
+  content   = "minecraft.${data.porkbun_domain.ddns_proxy.domain}"
 }
 
-resource "porkbun_nameservers" "sean_directory" {
-  domain      = "sean.directory"
+resource "porkbun_nameservers" "ddns_proxy" {
+  domain      = data.porkbun_domain.ddns_proxy.domain
   nameservers = data.dns_ns_record_set.desec_io.nameservers
+}
+
+data "porkbun_domain" "ddns_proxy" {
+  domain = "sean.directory"
 }
 
 data "dns_ns_record_set" "desec_io" {
