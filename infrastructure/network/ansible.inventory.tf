445b7189aec7d834fc3f7bccde0f1e5668247632
Sean Christians <sean.christians@vention.cc>
2026-05-26 16:49:09 -0400

    Fix references in Ansible inventory

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -15,12 +15,12 @@ locals {
       ansible_host           = data.tailscale_device.squiggle_darkened.name
       ansible_user           = "root"
       ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
-      wireguard_domain       = "${porkbun_dns_record.minecraft_server.subdomain}.${porkbun_dns_record.minecraft_server.domain}"
+      wireguard_domain       = "${porkbun_dns_record.minecraft_server_vpn.subdomain}.${porkbun_dns_record.minecraft_server_vpn.domain}"
       wireguard_dns          = "149.112.121.10,149.112.122.10,2620:10A:80BB::10,2620:10A:80BC::10" # CIRA Canadian Shield
       wireguard_ipv4_cidr    = "172.30.0.0/24"
       wireguard_ipv6_cidr    = "fdef:aced::/64"
-      desec_domain           = porkbun_dns_record.minecraft_server.content
-      desec_token            = terraform_data.minecraft_domain_desec_token.store.sensitive_output
+      desec_domain           = porkbun_dns_record.minecraft_server_vpn.content
+      desec_token            = terraform_data.minecraft_vpn_domain_desec_token.store.sensitive_output
       tailscale_auth_key     = tailscale_tailnet_key.container.key
       tailnet_service        = tailscale_service.minecraft.id
     }
