e1d87d0e210597b2aea3d36c76f3fc07fa85d5b4
Sean Christians <sean.christians@vention.cc>
2026-05-26 13:10:11 -0400

    Minecraft server (#230)

--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -17,6 +17,13 @@ action "ansible_playbook_run" "ddns" {
   }
 }
 
+action "ansible_playbook_run" "minecraft" {
+  config {
+    playbooks   = ["ansible-playbooks/minecraft.yaml"]
+    inventories = [local.ansible_inventory_encoded]
+  }
+}
+
 resource "terraform_data" "wg_easy_ansible_playbook" {
   store {
     input = [
@@ -53,10 +60,32 @@ resource "terraform_data" "ddns_ansible_playbook" {
   }
 }
 
+resource "terraform_data" "minecraft_ansible_playbook" {
+  store {
+    input = [
+      "319A3378-3724-4661-B33A-15D3EC29B57E",
+      data.local_file.minecraft_ansible_playbook.id,
+      local.ansible_inventory["minecraft_servers"]
+    ]
+    sensitive = true
+  }
+
+  lifecycle {
+    action_trigger {
+      actions = [action.ansible_playbook_run.minecraft]
+      events  = [after_create, after_update]
+    }
+  }
+}
+
 data "local_file" "wg_easy_ansible_playbook" {
   filename = "ansible-playbooks/wg-easy.yaml"
 }
 
 data "local_file" "ddns_ansible_playbook" {
   filename = "ansible-playbooks/ddns.yaml"
+}
+
+data "local_file" "minecraft_ansible_playbook" {
+  filename = "ansible-playbooks/minecraft.yaml"
 }
