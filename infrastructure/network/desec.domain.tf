207ee6d03e5e442379d09e4894935a1f92fa3896
Sean Christians <sean.christians@vention.cc>
2026-05-26 02:51:14 -0400

    Move deSEC domain to separate file

--- infrastructure/network/desec.domain.tf
+++ infrastructure/network/desec.domain.tf
@@ -1,9 +1,4 @@
-resource "porkbun_dns_record" "minecraft_server" {
-  domain    = "seanchristians.ca"
-  subdomain = "minecraft"
-  type      = "CNAME"
-  content   = "minecraft.${data.porkbun_domain.ddns_proxy.domain}"
-}
+# DNS proxy domain sean.directory at deSEC.io
 
 resource "porkbun_nameservers" "ddns_proxy" {
   domain      = data.porkbun_domain.ddns_proxy.domain
