f7bd8acb1b6295dcb17ba3afdcec7786f2c1cb2e
Sean Christians <sean.christians@vention.cc>
2026-05-26 17:23:25 -0400

    desec dnssec records for porkbun

--- infrastructure/network/desec.domain.tf
+++ infrastructure/network/desec.domain.tf
@@ -3,6 +3,12 @@ resource "porkbun_nameservers" "dns_proxy" {
   nameservers = [for ns in data.desec_rrset.dns_proxy_nameservers.rdata : trimsuffix(ns, ".")]
 }
 
+resource "porkbun_dnssec_record" "dns_proxy" {
+  domain   = data.porkbun_domain.dns_proxy.domain
+  ds_data  = desec_domain.dns_proxy.keys.ds
+  key_data = desec_domain.dns_proxy.keys.dnskey
+}
+
 resource "desec_domain" "dns_proxy" {
   name = data.porkbun_domain.dns_proxy.domain
 }
