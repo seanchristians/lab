3a55e73aa4146893a7cc6556115f38cb8824b140
Sean Christians <sean.christians@vention.cc>
2026-05-26 03:11:58 -0400

    typo 2

--- infrastructure/network/minecraft.domains.tf
+++ infrastructure/network/minecraft.domains.tf
@@ -1,7 +1,7 @@
 module "minecraft_vpn_domain" {
   source = "../../terraform-modules/dns_proxy"
 
-  domain          = data.porkbun_domains.canada.domain
+  domain          = data.porkbun_domain.canada.domain
   subdomain       = "minecraft"
   proxy_subdomain = "minecraft"
 }
