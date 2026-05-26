7d9af69bfab4a47e4caf492cfc81facfa326861d
Sean Christians <sean.christians@vention.cc>
2026-05-26 18:07:45 -0400

    Switch to minecraft.sean.directory as VPN domain

--- infrastructure/network/minecraft.domains.tf
+++ infrastructure/network/minecraft.domains.tf
@@ -1,6 +1,3 @@
-resource "porkbun_dns_record" "minecraft_server_vpn" {
-  domain    = local.domain_canada
-  subdomain = "mc-vpn"
-  type      = "CNAME"
-  content   = "mc-vpn.${data.porkbun_domain.dns_proxy.domain}"
+locals {
+  minecraft_server_vpn_subdomain = "minecraft"
 }
