9e47632bf214ac29dad9f2385787ffcdaa46885c
Sean Christians <sean.christians@vention.cc>
2026-05-26 10:23:56 -0400

    Remove tailscale minecraft service

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -14,8 +14,6 @@ locals {
       ansible_host           = data.tailscale_device.squiggle_darkened.name
       ansible_user           = "root"
       ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
-      tailscale_auth_key     = tailscale_tailnet_key.container.key
-      tailnet_service        = tailscale_service.minecraft.id
       wireguard_domain       = "${porkbun_dns_record.minecraft_server.subdomain}.${porkbun_dns_record.minecraft_server.domain}"
       wireguard_dns          = "149.112.121.10,149.112.122.10,2620:10A:80BB::10,2620:10A:80BC::10" # CIRA Canadian Shield
       wireguard_ipv4_cidr    = "172.30.0.0/24"
