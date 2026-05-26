7932af93116b72778bc25ec32a493b429afd0e95
Sean Christians <sean.christians@vention.cc>
2026-05-26 16:42:43 -0400

    Move minecraft dns records to separate file

--- infrastructure/network/desec.domain.tf
+++ infrastructure/network/desec.domain.tf
@@ -1,10 +1,3 @@
-resource "porkbun_dns_record" "minecraft_server" {
-  domain    = "seanchristians.ca"
-  subdomain = "minecraft"
-  type      = "CNAME"
-  content   = "minecraft.${data.porkbun_domain.ddns_proxy.domain}"
-}
-
 resource "porkbun_nameservers" "ddns_proxy" {
   domain      = data.porkbun_domain.dns_proxy.domain
   nameservers = [for ns in data.desec_rrset.dns_proxy_nameservers.rdata : trimsuffix(ns, ".")]
