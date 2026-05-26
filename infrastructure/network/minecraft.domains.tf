ddbe7ce3a70fdce287492439c6c25f888a9c0571
Sean Christians <sean.christians@vention.cc>
2026-05-26 18:10:26 -0400

    DNS records for mc server

--- infrastructure/network/minecraft.domains.tf
+++ infrastructure/network/minecraft.domains.tf
@@ -1,3 +1,17 @@
 locals {
   minecraft_server_vpn_subdomain = "minecraft"
 }
+
+resource "porkbun_dns_record" "minecraft_server_a" {
+  domain    = local.domain_canada
+  subdomain = "minecraft"
+  type      = "A"
+  content   = "172.30.0.1"
+}
+
+resource "porkbun_dns_record" "minecraft_server_aaaa" {
+  domain    = local.domain_canada
+  subdomain = "minecraft"
+  type      = "AAAA"
+  content   = "fdef:aced::1"
+}
