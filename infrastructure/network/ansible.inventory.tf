681820a22f13f899e00e98eefc70d8204854d1af
Sean Christians <sean.christians@vention.cc>
2026-05-28 12:04:07 -0400

    Run Minecraft in a rootless Podman container (#238)

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -13,7 +13,7 @@ locals {
   ansible_hosts = {
     "squiggle-darkened" = {
       ansible_host           = data.tailscale_device.squiggle_darkened.name
-      ansible_user           = "root"
+      ansible_user           = "core"
       ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
       wireguard_domain       = "${local.minecraft_server_vpn_subdomain}.${desec_domain.dns_proxy.name}"
       wireguard_dns          = "149.112.121.10,149.112.122.10,2620:10A:80BB::10,2620:10A:80BC::10" # CIRA Canadian Shield
@@ -29,4 +29,18 @@ locals {
 
 data "tailscale_device" "squiggle_darkened" {
   hostname = "squiggle-darkened"
+}
+
+resource "ansible_group" "minecraft_servers" {
+  name     = "minecraft_servers"
+  children = [ansible_host.squiggle_darkened.id]
+}
+
+resource "ansible_host" "squiggle_darkened" {
+  name = "squiggle-darkened"
+  variables = {
+    ansible_host           = data.tailscale_device.squiggle_darkened.name
+    ansible_user           = "core"
+    ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
+  }
 }
--- infrastructure/network/ansible.playbooks.tf
+++ infrastructure/network/ansible.playbooks.tf
@@ -19,62 +19,59 @@ action "ansible_playbook_run" "ddns" {
 
 action "ansible_playbook_run" "minecraft" {
   config {
-    playbooks   = ["ansible-playbooks/minecraft.yaml"]
-    inventories = [local.ansible_inventory_encoded]
+    playbooks = ["ansible-playbooks/minecraft.yaml"]
   }
 }
 
-resource "terraform_data" "wg_easy_ansible_playbook" {
-  store {
-    input = [
-      "B2086939-7E82-446A-BAF9-98FCF7F193D3",
-      data.local_file.wg_easy_ansible_playbook.id,
-      local.ansible_inventory["wireguard_servers"]
-    ]
-    sensitive = true
-  }
+# resource "terraform_data" "wg_easy_ansible_playbook" {
+#   store {
+#     input = [
+#       "B2086939-7E82-446A-BAF9-98FCF7F193D3",
+#       data.local_file.wg_easy_ansible_playbook.id,
+#       local.ansible_inventory["wireguard_servers"]
+#     ]
+#     sensitive = true
+#   }
 
-  lifecycle {
-    action_trigger {
-      actions = [action.ansible_playbook_run.wg_easy]
-      events  = [after_create, after_update]
-    }
-  }
-}
+#   lifecycle {
+#     action_trigger {
+#       actions = [action.ansible_playbook_run.wg_easy]
+#       events  = [after_create, after_update]
+#     }
+#   }
+# }
 
-resource "terraform_data" "ddns_ansible_playbook" {
-  store {
-    input = [
-      "319A3378-3724-4661-B33A-15D3EC29B57E",
-      data.local_file.ddns_ansible_playbook.id,
-      local.ansible_inventory["ddns"]
-    ]
-    sensitive = true
-  }
+# resource "terraform_data" "ddns_ansible_playbook" {
+#   store {
+#     input = [
+#       "319A3378-3724-4661-B33A-15D3EC29B57E",
+#       data.local_file.ddns_ansible_playbook.id,
+#       local.ansible_inventory["ddns"]
+#     ]
+#     sensitive = true
+#   }
 
-  lifecycle {
-    action_trigger {
-      actions = [action.ansible_playbook_run.ddns]
-      events  = [after_create, after_update]
-    }
-  }
-}
+#   lifecycle {
+#     action_trigger {
+#       actions = [action.ansible_playbook_run.ddns]
+#       events  = [after_create, after_update]
+#     }
+#   }
+# }
 
 resource "terraform_data" "minecraft_ansible_playbook" {
-  store {
-    input = [
-      "319A3378-3724-4661-B33A-15D3EC29B57E",
-      data.local_file.minecraft_ansible_playbook.id,
-      local.ansible_inventory["minecraft_servers"]
-    ]
-    sensitive = true
-  }
+  input = [
+    "B2086939-7E82-446A-BAF9-98FCF7F193D3",
+    data.local_file.minecraft_ansible_playbook.id
+  ]
 
   lifecycle {
     action_trigger {
       actions = [action.ansible_playbook_run.minecraft]
       events  = [after_create, after_update]
     }
+
+    replace_triggered_by = [ansible_group.minecraft_servers]
   }
 }
 
