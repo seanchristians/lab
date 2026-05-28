34125ca83b3ddc448049e6efc0fcb66aaaf562e7
Sean Christians <sean.christians@vention.cc>
2026-05-28 10:40:31 -0400

    ansible provider-native inventory

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -30,3 +30,17 @@ locals {
 data "tailscale_device" "squiggle_darkened" {
   hostname = "squiggle-darkened"
 }
+
+resource "ansible_group" "minecraft_servers" {
+  name     = "minecraft_servers"
+  children = [ansible_host.squiggle_darkened.id]
+}
+
+resource "ansible_host" "squiggle_darkened" {
+  name = "squiggle-darkened"
+  variables = {
+    ansible_host           = data.tailscale_device.squiggle_darkened.name
+    ansible_user           = "core"
+    ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
+  }
+}
