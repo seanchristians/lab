9c5aac409ba059faee849815f8ce0029a9e89210
Sean Christians <sean.christians@vention.cc>
2026-05-28 13:19:13 -0400

    Assign ansible group in host resource

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -1,10 +1,10 @@
 resource "ansible_group" "minecraft_servers" {
-  name     = "minecraft_servers"
-  children = [ansible_host.squiggle_darkened.id]
+  name = "minecraft_servers"
 }
 
 resource "ansible_host" "squiggle_darkened" {
-  name = "squiggle-darkened"
+  name   = "squiggle-darkened"
+  groups = [ansible_group.minecraft_servers.id]
   variables = {
     ansible_host           = data.tailscale_device.squiggle_darkened.name
     ansible_user           = "core"
