e9ad25d2c57fbc846ea0be12e68d9b205473849a
Sean Christians <sean.christians@vention.cc>
2026-05-28 13:21:11 -0400

    Move Ansible playbook management out of Terraform (#239)
    The official Terraform provider for Ansible doesn't manage playbooks
    well. Instead, I think it's easier to use native Ansible for this piece.

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -1,43 +1,10 @@
-locals {
-  ansible_inventory_encoded = jsonencode(local.ansible_inventory)
-  ansible_inventory = sensitive({ for group, hosts in local.ansible_groups : group => { "hosts" = {
-    for host in hosts : host => local.ansible_hosts[host]
-  } } })
-
-  ansible_groups = {
-    wireguard_servers = ["squiggle-darkened"],
-    ddns              = ["squiggle-darkened"],
-    minecraft_servers = ["squiggle-darkened"]
-  }
-
-  ansible_hosts = {
-    "squiggle-darkened" = {
-      ansible_host           = data.tailscale_device.squiggle_darkened.name
-      ansible_user           = "core"
-      ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
-      wireguard_domain       = "${local.minecraft_server_vpn_subdomain}.${desec_domain.dns_proxy.name}"
-      wireguard_dns          = "149.112.121.10,149.112.122.10,2620:10A:80BB::10,2620:10A:80BC::10" # CIRA Canadian Shield
-      wireguard_ipv4_cidr    = "172.30.0.0/24"
-      wireguard_ipv6_cidr    = "fdef:aced::/64"
-      desec_domain           = "${local.minecraft_server_vpn_subdomain}.${desec_domain.dns_proxy.name}"
-      desec_token            = terraform_data.minecraft_vpn_domain_desec_token.store.sensitive_output
-      tailscale_auth_key     = tailscale_tailnet_key.container.key
-      tailnet_service        = tailscale_service.minecraft.id
-    }
-  }
-}
-
-data "tailscale_device" "squiggle_darkened" {
-  hostname = "squiggle-darkened"
-}
-
 resource "ansible_group" "minecraft_servers" {
-  name     = "minecraft_servers"
-  children = [ansible_host.squiggle_darkened.id]
+  name = "minecraft_servers"
 }
 
 resource "ansible_host" "squiggle_darkened" {
-  name = "squiggle-darkened"
+  name   = "squiggle-darkened"
+  groups = [ansible_group.minecraft_servers.id]
   variables = {
     ansible_host           = data.tailscale_device.squiggle_darkened.name
     ansible_user           = "core"
