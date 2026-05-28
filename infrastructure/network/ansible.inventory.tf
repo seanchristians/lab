8861a19ac508c125cf7d37791f3c9ec72ed1a287
Sean Christians <sean.christians@vention.cc>
2026-05-28 15:49:28 -0400

    Configure squiggle-darkened for DDNS (#247)

--- infrastructure/network/ansible.inventory.tf
+++ infrastructure/network/ansible.inventory.tf
@@ -2,12 +2,27 @@ resource "ansible_group" "minecraft_servers" {
   name = "minecraft_servers"
 }
 
+resource "ansible_group" "ddns" {
+  name = "ddns"
+
+  variables = {
+    desec_domain = desec_domain.dns_proxy.name
+  }
+}
+
 resource "ansible_host" "squiggle_darkened" {
-  name   = "squiggle-darkened"
-  groups = [ansible_group.minecraft_servers.id]
+  name = "squiggle-darkened"
+
+  groups = [
+    ansible_group.minecraft_servers.id,
+    ansible_group.ddns.id
+  ]
+
   variables = {
     ansible_host           = data.tailscale_device.squiggle_darkened.name
     ansible_user           = "core"
     ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
+    subdomain              = local.minecraft_server_vpn_subdomain
+    desec_token            = terraform_data.minecraft_vpn_domain_desec_token.store.sensitive_output
   }
 }
