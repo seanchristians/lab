a371e6b918437fac86df6a8825a26713add6306f
Sean Christians <sean.christians@vention.cc>
2026-06-28 12:28:21 -0400

    Set minecraft domain from var

--- infrastructure/minecraft.domains.tf
+++ infrastructure/minecraft.domains.tf
@@ -1,10 +1,10 @@
 locals {
-  minecraft_server_vpn_subdomain = "minecraft"
+  minecraft_server_dns_proxy_domain = join(".", [var.minecraft_server_device, local.dns_proxy_domain])
 }
 
 resource "porkbun_dns_record" "minecraft_server" {
   domain    = local.domain_canada
   subdomain = "minecraft"
   type      = "CNAME"
-  content   = "${local.minecraft_server_vpn_subdomain}.${desec_domain.dns_proxy.name}"
+  content   = local.minecraft_server_dns_proxy_domain
 }
