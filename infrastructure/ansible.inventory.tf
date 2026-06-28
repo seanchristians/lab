1aa95c6636c4ed64b8af8f7ad79e473f0fe35f10
Sean Christians <sean.christians@vention.cc>
2026-06-28 00:20:11 -0400

    Parse tailnet fqdn directly in ansible inventory

--- infrastructure/ansible.inventory.tf
+++ infrastructure/ansible.inventory.tf
@@ -1,12 +1,10 @@
 locals {
   ansible_inventory = { for group, group_data in var.ansible_groups : group => merge(group_data, {
-    "hosts" = { for host, host_data in local.ansible_hosts_with_fqdn : host => host_data
+    "hosts" = { for host, host_data in var.ansible_hosts : host => merge({
+      ansible_host = data.tailscale_device.ansible_host[host].name
+      }, host_data)
     if contains(keys(group_data.hosts), host) }
   }) }
-
-  ansible_hosts_with_fqdn = { for host, data in var.ansible_hosts : host => merge({
-    ansible_host = data.tailscale_device.ansible_host[host].name
-  }, data) }
 }
 
 ephemeral "local_command" "ansible_inventory" {
