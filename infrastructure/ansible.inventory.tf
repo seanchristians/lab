93921bc0d2deff92ea9f25b58f5ee2cc23daa4c0
Sean Christians <sean.christians@vention.cc>
2026-06-28 00:40:24 -0400

    Add extra group data from Terraform

--- infrastructure/ansible.inventory.tf
+++ infrastructure/ansible.inventory.tf
@@ -1,10 +1,20 @@
 locals {
-  ansible_inventory = { for group, group_data in var.ansible_groups : group => merge(group_data, {
-    "hosts" = { for host, host_data in var.ansible_hosts : host => merge({
-      ansible_host = data.tailscale_device.ansible_host[host].name
-      }, host_data)
-    if contains(group_data.hosts, host) }
+  ansible_inventory = { for group, group_data in var.ansible_groups : group => merge(group_data,
+    try(local.ansible_group_data[group], {}),
+    {
+      "hosts" = { for host, host_data in var.ansible_hosts : host => merge({
+        ansible_host = data.tailscale_device.ansible_host[host].name
+        }, host_data)
+      if contains(group_data.hosts, host) }
   }) }
+
+  ansible_group_data = {
+    ddns = {
+      desec_domain = local.dns_proxy_domain
+      subdomain    = local.minecraft_server_vpn_subdomain
+      desec_token  = terraform_data.minecraft_vpn_domain_desec_token.store.sensitive_output
+    }
+  }
 }
 
 ephemeral "local_command" "ansible_inventory" {
