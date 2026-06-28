5e877e34a92dd3d728edaf2e10f6e08ebd2752e1
Sean Christians <sean.christians@vention.cc>
2026-06-28 00:49:46 -0400

    Set ddns vars in local file

--- infrastructure/ansible.inventory.tf
+++ infrastructure/ansible.inventory.tf
@@ -1,20 +1,11 @@
 locals {
   ansible_inventory = { for group, group_data in var.ansible_groups : group => merge(group_data,
-    try(local.ansible_group_data[group], {}),
     {
       "hosts" = { for host, host_data in var.ansible_hosts : host => merge({
         ansible_host = data.tailscale_device.ansible_host[host].name
         }, host_data)
       if contains(group_data.hosts, host) }
   }) }
-
-  ansible_group_data = {
-    ddns = {
-      desec_domain = local.dns_proxy_domain
-      subdomain    = local.minecraft_server_vpn_subdomain
-      desec_token  = terraform_data.minecraft_vpn_domain_desec_token.store.sensitive_output
-    }
-  }
 }
 
 ephemeral "local_command" "ansible_inventory" {
@@ -26,4 +17,21 @@ ephemeral "local_command" "ansible_inventory" {
 data "tailscale_device" "ansible_host" {
   for_each = toset(keys(var.ansible_hosts))
   hostname = each.key
+}
+
+ephemeral "local_command" "ansible_vars_folders" {
+  command   = "mkdir"
+  arguments = ["group_vars", "host_vars"]
+}
+
+ephemeral "local_command" "ansible_group_ddns_vars" {
+  command   = "tee"
+  arguments = ["group_vars/ddns.yaml"]
+  stdin = yamlencode({
+    desec_domain = local.dns_proxy_domain
+    subdomain    = local.minecraft_server_vpn_subdomain
+    desec_token  = terraform_data.minecraft_vpn_domain_desec_token.store.sensitive_output
+  })
+
+  depends_on = [ephemeral.local_command.ansible_vars_folders]
 }
