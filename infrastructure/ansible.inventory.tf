07702055ffff3ada43a952ef214294b9c9e1c360
Sean Christians <sean.christians@vention.cc>
2026-06-27 21:45:48 -0400

    Remove native ansible hosts and groups

--- infrastructure/ansible.inventory.tf
+++ infrastructure/ansible.inventory.tf
@@ -1,32 +1,3 @@
-resource "ansible_group" "minecraft_servers" {
-  name = "minecraft_servers"
-}
-
-resource "ansible_group" "ddns" {
-  name = "ddns"
-
-  variables = {
-    desec_domain = desec_domain.dns_proxy.name
-  }
-}
-
-resource "ansible_host" "squiggle_darkened" {
-  name = "squiggle-darkened"
-
-  groups = [
-    ansible_group.minecraft_servers.id,
-    ansible_group.ddns.id
-  ]
-
-  variables = {
-    ansible_host           = data.tailscale_device.squiggle_darkened.name
-    ansible_user           = "core"
-    ansible_ssh_extra_args = "-o StrictHostKeyChecking=no"
-    subdomain              = local.minecraft_server_vpn_subdomain
-    desec_token            = terraform_data.minecraft_vpn_domain_desec_token.store.sensitive_output
-  }
-}
-
 resource "ansible_playbook" "minecraft_server" {
   for_each   = toset(try(var.ansible_groups["minecraft_servers"]), [])
   playbook   = "playbooks/minecraft.yaml"
