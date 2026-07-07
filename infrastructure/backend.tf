46e54536d35aed4de763d47f86d4129c926f25f0
Sean Christians <sean.christians@vention.cc>
2026-07-06 22:51:15 -0400

    Use Terraform state data to bypass cyclical dependency rules (#318)
    I only want to run the Ansible playbook if the diff shows that changes
    are necessary. Terraform doesn't have a way to update resources based on
    a conditional, it can only detect if a value has "changed."
    
    This method sets a UUID for a terraform_data resource and stores it as a
    module output in state. If the conditional check returns true, it
    generates a new UUID and runs the playbook. If the conditional is false,
    it reads its own value back from the Terraform state file and doesn't
    trigger a new run.
    
    This isn't possible with a simple `locals` value because Terraform will
    reject the cyclical dependency.

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -5,3 +5,11 @@ terraform {
     use_lockfile = true
   }
 }
+
+data "terraform_remote_state" "this" {
+  backend = "s3"
+  config = {
+    bucket = var.backend_s3_bucket
+    key    = "infrastructure/network/terraform.tfstate"
+  }
+}
