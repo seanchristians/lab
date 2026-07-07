96a01205925990c618da738d469ef835902178f5
Sean Christians <sean.christians@vention.cc>
2026-07-06 22:44:17 -0400

    Bypass cyclical dependency by pulling value from backend data

--- infrastructure/ansible.playbooks.tf
+++ infrastructure/ansible.playbooks.tf
@@ -1,7 +1,7 @@
 resource "terraform_data" "ansible_playbook_run" {
-  for_each = data.local_command.ansible_playbook_diff
+  for_each = terraform_data.ansible_playbook_diff_id
 
-  triggers_replace = each.value.stdout ? uuid() : null
+  triggers_replace = each.value.output
 
   provisioner "local-exec" {
     environment = {
@@ -9,6 +9,16 @@ resource "terraform_data" "ansible_playbook_run" {
     }
     command = "ansible-playbook \"$PLAYBOOK\""
   }
+}
+
+output "ansible_playbook_diff_id" {
+  value = { for key, value in terraform_data.ansible_playbook_diff_id : key => value.output }
+}
+
+resource "terraform_data" "ansible_playbook_diff_id" {
+  for_each = data.local_command.ansible_playbook_diff
+
+  input = each.value.stdout ? uuid() : try(data.terraform_remote_state.this.outputs.ansible_playbook_diff_id[each.key], uuid())
 }
 
 data "local_command" "ansible_playbook_diff" {
